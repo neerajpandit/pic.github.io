9f35125848679164e6e506f83653c59a13bb2beb
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-07-30 18:53:51 +0530

    added static

--- picupload/settings.py
+++ picupload/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
-
+STAIC_ROOT = BASE_DIR,'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- picupload/urls.py
+++ picupload/urls.py
@@ -21,4 +21,4 @@ from picapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home)
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
